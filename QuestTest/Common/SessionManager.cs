90807ad72e6147669a6d2086ce266b86f052ab37
Soloria Wesp <gckain@mail.ru>
2018-08-14 01:44:18 +0300

    add session manager

---
+++ QuestTest/Common/SessionManager.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace QuestTest
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    public static class SessionManager
+    {
+        private static IWebDriver _driver;
+
+        public static void Open(string path)
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Url = path;
+        }
+
+        public static void Close()
+        {
+            _driver.Quit();
+        }
+
+        public static string Path = "http://blog.csssr.ru/qa-engineer/";
+
+        public static IWebDriver getDriver() => _driver;
+
+    }
+}
