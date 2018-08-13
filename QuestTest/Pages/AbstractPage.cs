c9ef555aa4842b6bcc6b20f60aa09d67b870cd4e
Soloria Wesp <gckain@mail.ru>
2018-08-14 01:50:00 +0300

    add abstract and main pages

---
+++ QuestTest/Pages/AbstractPage.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace QuestTest
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+
+    public abstract class AbstractPage
+    {
+        private readonly IWebDriver driver;
+
+        protected AbstractPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public IWebDriver getDriver() => driver;
+    }
+}
---
+++ QuestTest/Pages/MainPage.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace QuestTest
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+    using JetBrains.Annotations;
+
+    public class MainPage : AbstractPage
+    {
+        public MainPage(IWebDriver driver) : base(driver) {}
+
+
+    }
+}
