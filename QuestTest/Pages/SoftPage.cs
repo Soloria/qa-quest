9240a2cc91f5678a9e73830594b4474589abdff4
Soloria Wesp <gckain@mail.ru>
2018-08-14 15:39:35 +0300

    add soft and vk pages

---
+++ QuestTest/Pages/SoftPage.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestTest
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+
+
+    public class SoftPage : AbstractPage
+    {
+        public SoftPage(IWebDriver driver) : base(driver) {}
+    }
+}
---
+++ QuestTest/Pages/VkPage.cs
@@ -0,0 +1,9 @@
+﻿namespace QuestTest
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+    public class VkPage : AbstractPage
+    {
+        public VkPage(IWebDriver driver) : base(driver) {}
+    }
+}
